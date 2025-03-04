ade2d16e84bb104cbe960dc3f08ec4872e184877
Anshi Patel <anshipatel881@gmail.com>
2025-03-04 11:35:44 +0530

    [Anshi] Add . Added find prime factors and store in array

---
+++ Arrays/PrimeFactors.js
@@ -0,0 +1,25 @@
+function getPrimeFactors(n) {
+    let factors = [];
+    
+    while (n % 2 === 0) {
+        factors.push(2);
+        n = n / 2;
+    }
+    
+    for (let i = 3; i <= Math.sqrt(n); i += 2) {
+        while (n % i === 0) {
+            factors.push(i);
+            n = n / i;
+        }
+    }
+    
+    if (n > 2) {
+        factors.push(n);
+    }
+    
+    return factors;
+}
+
+const number = 56; 
+const primeFactors = getPrimeFactors(number);
+console.log(`Prime factors of ${number}:`, primeFactors);
